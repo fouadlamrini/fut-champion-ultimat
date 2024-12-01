77399bb90e2cfd014f86b430de69a83f7107811c
fouad lmrini <fouad11.lamrini@gmail.com>
2024-12-01 23:09:38 +0100

    ajoute les joueur dans le terrain

--- script/js.js
+++ script/js.js
@@ -1,7 +1,8 @@
+let joueurs = document.querySelectorAll(".terrain > div");
 document.getElementById("formation").addEventListener("change", function () {
     let value = this.value;
 
-    let joueurs = document.querySelectorAll(".terrain > div");
+   
 
     if (value == "4-3-3") {
         //joueur11
@@ -23,57 +24,57 @@ document.getElementById("formation").addEventListener("change", function () {
         joueurs[5].style.top = "-75px";
         joueurs[5].style.left = "250px";
         //joueur5
-        joueurs[6].style.top = " 5px";
-        joueurs[6].style.left = "780px";
+        joueurs[6].style.top = "3.3%";
+        joueurs[6].style.left = "88%";
         //joueur4
-        joueurs[7].style.top = "50px";
-        joueurs[7].style.left = "540px";
+        joueurs[7].style.top = "7.7%";
+        joueurs[7].style.left = "63%";
         //joueur3
-        joueurs[8].style.left = "300px";
+        joueurs[8].style.left = "32%";
         //joueur2
-        joueurs[9].style.top = "-140px";
-        joueurs[9].style.left = "50px";
+        joueurs[9].style.top = "-20%";
+        joueurs[9].style.left = "5%";
         //joueur1
-        joueurs[10].style.top = "40px";
-        joueurs[10].style.left = "430px";
+        joueurs[10].style.top = "5%";
+        joueurs[10].style.left = "48%";
 
     }
 
     else if (value == "4-4-2") {
         //joueur11
-        joueurs[0].style.top = "55px";
-        joueurs[0].style.left = "480px";
+        joueurs[0].style.top = "8%";
+        joueurs[0].style.left = "57%";
         // joueur10
-        joueurs[1].style.left = "350px";
+        joueurs[1].style.left = "40%";
 
         // joueur9
-        joueurs[2].style.top = "20px";
-        joueurs[2].style.left = "650px";
+        joueurs[2].style.top = "3%";
+        joueurs[2].style.left = "80%";
 
         // joueur8
-        joueurs[3].style.left = "520px";
-        joueurs[3].style.top = "25px";
+        joueurs[3].style.left = "60%";
+        joueurs[3].style.top = "2.8%";
         // joueur7
-        joueurs[4].style.top = "-25px";
-        joueurs[4].style.left = "330px";
+        joueurs[4].style.top = "-5%";
+        joueurs[4].style.left = "37%";
         // joueur6
-        joueurs[5].style.top = "-140px";
-        joueurs[5].style.left = "190px";
+        joueurs[5].style.top = "-20%";
+        joueurs[5].style.left = "15%";
         // joueur5
-        joueurs[6].style.top = " 5px";
-        joueurs[6].style.left = "780px";
+        joueurs[6].style.top = "3.3%";
+        joueurs[6].style.left = "88%";
         // joueur4
 
-        joueurs[7].style.top = "50px";
-        joueurs[7].style.left = "540px";
+        joueurs[7].style.top = "7.7%";
+        joueurs[7].style.left = "63%";
         // joueur3
-        joueurs[8].style.left = "300px";
+        joueurs[8].style.left = "32%";
         // joueur2
-        joueurs[9].style.top = "-140px";
-        joueurs[9].style.left = "50px";
+        joueurs[9].style.top = "-20%";
+        joueurs[9].style.left = "5%";
         // joueur1
-        joueurs[10].style.top = "40px";
-        joueurs[10].style.left = "430px";
+        joueurs[10].style.top = "5%";
+        joueurs[10].style.left = "48%";
 
     }
 });
@@ -82,13 +83,26 @@ document.getElementById("formation").addEventListener("change", function () {
 // affiche la forme
 let form = document.getElementById("form");
 function afficheForm() {
+
     form.style.display = "block";
 }
+
 // ajouter le carde
 
-function ajouter() {
-    if (!inputRating.value == "" && !inputImgp.value == "" && !inputNom.value == "" && !inputNom.value == "" && !inputPace.value == "" && !inputShooting.value == "" && !inputPassing.value == "" && !inputDribbling.value == "" &&!inputDefending.value==""&&!inputPhysical.value==""&&!inputImgf.value==""&&! inputImgc.value==""  ) {
-        event.preventDefault();
+function ajouter() {event.preventDefault();
+    document.getElementById("inputRating").style.borderBlockColor="";
+    document.getElementById("inputImgp").style.borderBlockColor="";
+    document.getElementById("inputNom").style.borderBlockColor="";
+    document.getElementById("inputPace").style.borderBlockColor="";
+    document.getElementById("inputShooting").style.borderBlockColor="";
+    document.getElementById("inputPassing").style.borderBlockColor="";
+    document.getElementById("inputDribbling").style.borderBlockColor="";
+    document.getElementById("inputDefending").style.borderBlockColor="";
+    document.getElementById("inputPhysical").style.borderBlockColor="";
+    document.getElementById("inputImgf").style.borderBlockColor="";
+    document.getElementById("inputImgc").style.borderBlockColor="";
+    if (!inputRating.value == "" && !inputImgp.value == "" && !inputNom.value == "" &&  !inputPace.value == "" && !inputShooting.value == "" && !inputPassing.value == "" && !inputDribbling.value == "" &&!inputDefending.value==""&&!inputPhysical.value==""&&!inputImgf.value==""&&! inputImgc.value==""  ) {
+        
         let allCard = document.getElementById("allCard");
         let inputRating = document.getElementById("inputRating").value;
         let select = document.getElementById("select").value;
@@ -155,15 +169,72 @@ function ajouter() {
         document.getElementById("inputImgf").value = "";
         document.getElementById("inputImgc").value = "";
     }
-    else if (inputRating.value == "" ){ inputRating.style.}
+    if (inputRating.value == "" ){ document.getElementById("inputRating").style.borderBlockColor="red";
+    }
+      if (inputImgp.value == "" ){ document.getElementById("inputImgp").style.borderBlockColor="red";
+    }
+      if (inputNom.value == "" ){ document.getElementById("inputNom").style.borderBlockColor="red";
+    }
+      if (inputPace.value == "" ){ document.getElementById("inputPace").style.borderBlockColor="red";
+    }
+      if (inputShooting.value == "" ){ document.getElementById("inputShooting").style.borderBlockColor="red";
+    }
+      if (inputPassing.value == "" ){ document.getElementById("inputPassing").style.borderBlockColor="red";
+    }
+      if (inputDribbling.value == "" ){ document.getElementById("inputDribbling").style.borderBlockColor="red";
+    }
+      if (inputDefending.value == "" ){ document.getElementById("inputDefending").style.borderBlockColor="red";
+    }
+      if (inputPhysical.value == "" ){ document.getElementById("inputPhysical").style.borderBlockColor="red";
+    }
+      if (inputImgf.value == "" ){ document.getElementById("inputImgf").style.borderBlockColor="red";
+    } 
+     if (inputImgc.value == "" ){ document.getElementById("inputImgc").style.borderBlockColor="red";
+    }
 
 
 
 
 }
+// annuler la forme de l'ajoute
+function annuler(){
+  event.preventDefault();
+  form.style.display = "none";
+}
 
 
 
+
+
+
+// ajoute les joueurs dans le terrain
+
+
+
+
+let selectionJoueur = null;
+joueurs.forEach(function (joueur) {
+    joueur.addEventListener("click", function () {
+        selectionJoueur = joueur;
+        joueur.style.backgroundColor="yellow";
+        
+    });
+});
+
+document.querySelectorAll(".allCard .card").forEach(function (card) {
+    card.addEventListener("click", function () {
+
+        if (selectionJoueur) {
+            const cardClone = card.cloneNode(true);
+            selectionJoueur.innerHTML = "";  
+            selectionJoueur.appendChild(cardClone); 
+            cardClone.style.transform = "scale(0.5)"; 
+            selectionJoueur = null; 
+        } else {
+            alert("il faut choisir le joueur");  
+        }
+    });
+});
 
 
 
